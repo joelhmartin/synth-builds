32bddb97526b7d067f378d822f32e3d7b8c60e55
joelhmartin <joelhmartin@gmail.com>
2024-01-05 12:21:02 -0500

    ssh added

--- src/components/newPost/NewPostError.tsx
+++ src/components/newPost/NewPostError.tsx
@@ -11,7 +11,6 @@ const NewPostError = ({ errorMessage, onClick }: Props) => {
       <Text className="text-lg font-bold">Something went wrong</Text>
       {errorMessage && <Text className="text-red-500">{errorMessage}</Text>}
       <Button
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
         onClick={onClick}
       >
         Try Again
